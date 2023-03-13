c745d72388757b54bad63e3922fbd716a5827316
Giorgio Daniele <giorgiodaniele15@gmail.com>
2023-03-13 09:55:27 +0100

    Code refactoring  on Forth exercise

--- lab-1/forth/src/main/kotlin/Forth.kt
+++ lab-1/forth/src/main/kotlin/Forth.kt
@@ -15,32 +15,49 @@ class Forth {
     private var SWAP : String = "swap"
     private var OVER : String = "over"
 
-    // It stores all the ovverided instructions and the new ones, as well
-    var userAliasInstructions   : MutableMap<String, LinkedList<String>> = mutableMapOf()
+    private val allNumber = Regex("[0-9]+")
+    private val allChars  = Regex("[A-z]+")
+
+    // It stores all the override instructions and the new ones, as well
+    private val renamedInstructions   : MutableMap<String, LinkedList<String>> = mutableMapOf()
     // It stores all the new variable
-    var userAliasVariables     : MutableMap<String, String> = mutableMapOf()
-    // It stores all the new typdef operations
-    var userAliasOperations    : MutableMap<String, String> = mutableMapOf()
+    private val userVariables      : MutableMap<String, String> = mutableMapOf()
+    // It stores all the new typedef operations
+    private val userOperations     : MutableMap<String, String> = mutableMapOf()
+
+    // If a given instruction contains just symbol and it belongs to Forth instructions set, but without parameters, it is an invalid instruction
+    val checkForInvalidInstruction : (String) -> Boolean = { instruction: String -> (!instruction.trim().contains(" ") && instruction.lowercase() in arrayOf(ADD, SUB, MUL, DIV, DUP, DROP, SWAP, OVER)) }
+    // If a given instruction contains just a symbol and it does not belong neither to Forth instructions set nor to alias instructions set, it is just a random word
+    val checkForInvalidSyntax      : (String) -> Boolean = { instruction: String -> (!instruction.trim().contains(" ") && instruction.lowercase() !in arrayOf(ADD, SUB, MUL, DIV, DUP, DROP, SWAP, OVER) && instruction.lowercase() !in userOperations) }
+    // If the user is writing a new definition, but it contains only numbers, it is an invalid action
+    val checkForInvalidDefinition  : (String) -> Boolean = { instruction: String -> instruction.trim().contains(":") && instruction.trim().split(" ").filter { symbol: String -> !(symbol == ":" || symbol == ";") }.all { symbol: String -> symbol.matches(allNumber) }}
+
+    // The parser works in this way: all the well-known symbols are collected into the second element in the Pair, while the rest is collected into the first element in the Pair
+    val instructionParser : (String) -> Pair<List<String>, List<String>> = { instruction: String -> instruction.split(" ").filter { syntax: String -> !(syntax == ":" || syntax == ";") }.map { syntax: String -> syntax.lowercase() }.partition { syntax: String -> syntax.lowercase() !in arrayOf(ADD, SUB, MUL, DIV, DROP, DUP, SWAP, OVER) }}
 
 
-    val isANumbersSequence : (List<String>) -> Boolean =  { stack : List<String> ->  stack.all { symbol : String -> symbol.lowercase() !in arrayOf(ADD, SUB, MUL, DIV, DROP, DUP, SWAP, OVER) && symbol.lowercase() !in userAliasInstructions && symbol !in userAliasVariables && symbol !in userAliasOperations}  }
-    val isAnEmptySequence  : (List<String>) -> Boolean =  { stack : List<String> ->  stack.isEmpty() }
+    val checkForSimpleNumberList : (List<String>) -> Boolean =  { stack : List<String> ->  stack.all { symbol : String -> symbol.lowercase().matches(allNumber)} }
+    val checkForEmptyList        : (List<String>) -> Boolean =  { stack : List<String> ->  stack.isEmpty() }
+
+    val isThisSymbolDigit        : (String) -> Boolean = { symbol: String -> symbol.lowercase() !in arrayOf(ADD, SUB, MUL, DIV, DUP, DROP, SWAP, OVER) && symbol.lowercase() !in renamedInstructions }
 
     val popFirstOperand  : (LinkedList<String>) -> Int = { stack : LinkedList<String> ->  try { stack.pop().toInt() } catch (exception: java.util.NoSuchElementException) { throw java.util.NoSuchElementException("empty stack") }}
     val popSecondOperand : (LinkedList<String>) -> Int = { stack : LinkedList<String> ->  try { stack.pop().toInt() } catch (exception: java.util.NoSuchElementException) { throw java.util.NoSuchElementException("only one value on the stack") }}
 
-    // Math operations
+
+    /*****************************************************************************************************************************************/
+    /*****************************************************************************************************************************************/
+    /* Forth operation definition */
+    /*****************************************************************************************************************************************/
+    /*****************************************************************************************************************************************/
+    // Math instructions
     val doADD  : (Int, Int, LinkedList<String>) -> Unit = { n1: Int, n2: Int, stack : LinkedList<String> -> stack.push((n1 + n2).toString()) }
     val doSUB  : (Int, Int, LinkedList<String>) -> Unit = { n1: Int, n2: Int, stack : LinkedList<String> -> stack.push((n1 - n2).toString()) }
     val doMUL  : (Int, Int, LinkedList<String>) -> Unit = { n1: Int, n2: Int, stack : LinkedList<String> -> stack.push((n1 * n2).toString()) }
     val doDIV  : (Int, Int, LinkedList<String>) -> Unit = { n1: Int, n2: Int, stack : LinkedList<String> -> if(n2 == 0) throw ArithmeticException("divide by zero") else stack.push((n1 / n2).toString()) }
-
     // Stack manipulation instructions
     val doDROP : (LinkedList<String>) -> Unit = { stack : LinkedList<String> ->  try { stack.pop().toInt() } catch (exception: java.util.NoSuchElementException) { throw java.util.NoSuchElementException("empty stack") }}
-
-    val doDUP : (LinkedList<String>) -> Unit  = {  stack : LinkedList<String> ->
-        if(stack.isEmpty()) throw java.util.NoSuchElementException("empty stack") else { /* Fetch the last value */ val value = stack.pop(); stack.push(value); stack.push(value) }}
-
+    val doDUP : (LinkedList<String>) -> Unit  = {  stack : LinkedList<String> ->  if(stack.isEmpty()) throw java.util.NoSuchElementException("empty stack") else { /* Fetch the last value */ val value = stack.pop(); stack.push(value); stack.push(value) }}
     val doSWAP : (LinkedList<String>) -> Unit = {  stack : LinkedList<String> ->
         if(stack.isEmpty()) throw java.util.NoSuchElementException("empty stack") else {
         if(stack.size < MIN_NUMBER_FOR_SWAP) throw java.util.NoSuchElementException("only one value on the stack") else {val firstOperand  = stack.pop(); val secondOperand = stack.pop(); stack.push(firstOperand); stack.push(secondOperand) }}
@@ -48,12 +65,14 @@ class Forth {
     val doOVER : (LinkedList<String>) -> Unit = {  stack : LinkedList<String> ->
         if(stack.isEmpty()) throw java.util.NoSuchElementException("empty stack") else {
             if(stack.size < MIN_NUMBER_FOR_OVER) throw java.util.NoSuchElementException("only one value on the stack") else { val firstOperand = stack.pop(); val secondOperand = stack.pop(); stack.push(secondOperand); stack.push(firstOperand); stack.push((secondOperand)) }}}
+    /*****************************************************************************************************************************************/
+    /*****************************************************************************************************************************************/
 
     private fun operatorDispatcher (stack: LinkedList<String>, symbol: String) : Unit {
 
         var symbolToProcess : String = symbol
         // If there is an override, use it
-        if(symbol.lowercase() in userAliasInstructions) symbolToProcess = userAliasInstructions[symbol.lowercase()].toString()
+        if(symbol.lowercase() in renamedInstructions) symbolToProcess = renamedInstructions[symbol.lowercase()].toString()
         // ...otherwise, use the default behaviour
 
         when(symbolToProcess.lowercase()) {
@@ -66,37 +85,30 @@ class Forth {
             DROP -> doDROP(stack)
             SWAP -> doSWAP(stack)
             OVER -> doOVER(stack)
-            // ...user instruction
             else -> {
-                // ...fetch the correspondent commands
-                val commands = userAliasInstructions[symbol.lowercase()] ?: emptyList()
+                // If it reaches this point, it means the user is performing a renamed instruction
+                val commands = renamedInstructions[symbol.lowercase()] ?: emptyList()
                 val input = LinkedList<String>(); input.addAll(commands)
                 // ...if they are already known, then recursive on this function
                 if(commands.all { syntax: String -> syntax in arrayOf(ADD, SUB, MUL, DIV, DROP, DUP, SWAP, OVER) })
                     commands.forEach { symbol: String -> operatorDispatcher(stack, symbol) }
                 else {
                     // Write here the new instructions and their own behavior
-                    if(symbol == "countup") { val values = userAliasInstructions[symbol.lowercase()] ?: emptyList(); for(value in commands) stack.push(value)}
+                    if(symbol == "countup") { val values = renamedInstructions[symbol.lowercase()] ?: emptyList(); for(value in commands) stack.push(value)}
                 }
             }
         }
     }
 
-    private fun evaluteCodeLine(code: List<String>) : List<Int> =
-
-        // If the code sequence is made of numbers only, return the list by itself
-        if (isANumbersSequence(code)) code.map { symbol : String -> symbol.toInt() }
-        // If the code sequence is empty, return an empty list
-        else if (isAnEmptySequence(code)) emptyList()
-        // Otherwise, process the code sequence
-        else {
+    private fun evaluteCodeLine(code: List<String>) : List<Int> =  if (checkForSimpleNumberList(code)) code.map { symbol : String -> symbol.toInt() }  else if (checkForEmptyList(code)) emptyList()  else {
 
         var symbolSequence : LinkedList<String> = LinkedList()
+
         for (symbol in code)
             // If the symbol is a number or a variable
-            if(symbol.lowercase() !in arrayOf(ADD, SUB, MUL, DIV, DUP, DROP, SWAP, OVER) && symbol.lowercase() !in userAliasInstructions) {
+            if(isThisSymbolDigit(symbol)) {
                 var symbolToStack : String = symbol
-                if(symbol in userAliasVariables) symbolToStack= userAliasVariables[symbol].toString()
+                if(symbol in userVariables) symbolToStack = userVariables[symbol].toString()
                 symbolSequence.push(symbolToStack)
             }
             // If the symbol is an instruction
@@ -109,75 +121,61 @@ class Forth {
 
         val resultList : LinkedList<Int> = LinkedList()
 
-        // Each line is an input to process
         for(instruction in line) {
-
-            // Check if there is just one string within the current instruction. If yes, and that string is a stack instruction, throw an exception
-            if(instruction.split(" ").count() == 1 &&
-                instruction.split(" ").first() in arrayOf(ADD, SUB, MUL, DIV, DUP, DROP, SWAP, OVER))
-                    throw IllegalArgumentException("empty stack")
-
-            // Check if there is just one string within the current instruction. If yes, and that string is just a random name, throw an exception
-            if(instruction.split(" ").count() == 1 &&
-                instruction.split(" ").first() !in arrayOf(ADD, SUB, MUL, DIV, DUP, DROP, SWAP, OVER) &&
-                instruction.split(" ").first() !in userAliasOperations)
-                throw IllegalArgumentException("undefined operation")
-
-            // In the case of an instruction to define a new one
-            if(instruction.contains(":") && instruction.split(" ")
-                    .filter { symbol: String -> !(symbol.equals(":") || symbol.equals(";")) }
-                    .all    { symbol: String -> symbol.matches(Regex("[0-9]+")) }) throw IllegalArgumentException("illegal operation")
-
-
-            //Check if it is an typedef instruction or not
+            // Check for the input
+            if(checkForInvalidInstruction(instruction)) throw IllegalArgumentException("empty stack")
+            if(checkForInvalidSyntax(instruction))      throw IllegalArgumentException("undefined operation")
+            if(checkForInvalidDefinition(instruction))  throw IllegalArgumentException("illegal operation")
             if(instruction.contains(":")) {
 
-                // This is a typedef instruction
-                val command : Pair<List<String>, List<String>> = instruction.split(" ")
-                    .filter { syntax: String -> !(syntax.equals(":") || syntax.equals(";")) }
-                    .map    { syntax: String -> syntax.lowercase() }
-                    .partition { syntax: String -> syntax.lowercase() !in arrayOf(ADD, SUB, MUL, DIV, DROP, DUP, SWAP, OVER) }
+                val command : Pair<List<String>, List<String>> = instructionParser(instruction)
 
-                    val leftChunk  = command.first    // It contains all the symbol which are not original
-                    val rightChunk = command.second   // It contains all the symbol which are original
+                    val unknownSymbols   = command.first
+                    val knownSymbols     = command.second
 
-                    // If leftChunk is empty, the user is modding an original operation (he is just aliasing an existing operation)
-                    if(leftChunk.isEmpty()) {
+                    // If there are no known symbols, it means all symbols are arleardy known. In practise, the user is defining
+                    // something like "+ *", "dup over", "over dup", and so on
+                    if(unknownSymbols.isEmpty()) {
                         val typedefCommands: LinkedList<String> = LinkedList()
-                        typedefCommands.addAll(rightChunk.subList(1,rightChunk.size))
-                        userAliasInstructions[rightChunk.first()] = typedefCommands
+                        typedefCommands.addAll(knownSymbols.subList(1,knownSymbols.size))
+                        renamedInstructions[knownSymbols.first()] = typedefCommands
                     }
-                    // If rightChuck is empty and leftChunk contains two elements (foo 5, bar foo, ...), the user is defining a variable
-                    else if(rightChunk.isEmpty() && leftChunk.size == 2) {
-                        val variableName  : String = leftChunk[0]
-                        val variableValue : String = leftChunk[1]
+
+                    // If there are no known symbols and there are just two unknown symbol, it means the user is creating a new
+                    // variable or overriding an existing variable with a new name
+                    else if(knownSymbols.isEmpty() && unknownSymbols.size == 2) {
+                        val variableName  : String = unknownSymbols[0]
+                        val variableValue : String = unknownSymbols[1]
                         // If variableValue is digit, the user is creating a variable
-                        if(variableValue.contains(Regex("[0-9]+"))) { userAliasVariables.put(variableName, variableValue) }
-                        // ...otherwise, the user is changing the name
-                        else if(variableValue in userAliasVariables) userAliasVariables.put(variableName, userAliasVariables.get(variableValue).toString())
+                        if(variableValue.contains(allNumber)) userVariables[variableName] = variableValue
+                        // ...otherwise, the user is replacing an existing name
+                        else if(variableValue in userVariables) userVariables[variableName] = userVariables[variableValue].toString()
                     }
-                    // If rightChunk is empty, and the first element within the leftChunk contains some chars, then the user
-                    // is defining a new instruction followed by its behavior
-                    else if(rightChunk.isEmpty() && leftChunk.first().matches(Regex("[A-z]+"))) {
+
+                    // If there are no known symbols, but the first known symbol is a literal, the user is definining a new
+                    // opeation, by providing its own behavior
+                    else if(knownSymbols.isEmpty() && unknownSymbols.first().matches(allChars)) {
                             val typedefCommands: LinkedList<String> = LinkedList()
-                            typedefCommands.addAll(leftChunk.subList(1, leftChunk.size))
-                            userAliasOperations[leftChunk.first()] = typedefCommands.joinToString(" ")
+                            typedefCommands.addAll(unknownSymbols.subList(1, unknownSymbols.size))
+                            userOperations[unknownSymbols.first()] = typedefCommands.joinToString(" ")
                     }
+
                     // If rightChunk is empty, but leftChunk is larger than 1, the user is defining an operation
-                    else if(rightChunk.isNotEmpty() && leftChunk.size > 2) {
-                        val variableName  : String = leftChunk[0]
-                        userAliasOperations.put(variableName, (leftChunk.subList(1, leftChunk.size) + rightChunk).joinToString(" "))
+                    else if(knownSymbols.isNotEmpty() && unknownSymbols.size > 2) {
+                        val variableName  : String = unknownSymbols[0]
+                        userOperations[variableName] = (unknownSymbols.subList(1, unknownSymbols.size) + knownSymbols).joinToString(" ")
                     }
+
                     // ...otherwise, the user is defining a new name for an original instruction or a chain of them
                     else {
                         val typedefCommands: LinkedList<String> = LinkedList()
-                        typedefCommands.addAll(rightChunk)
-                        userAliasInstructions[leftChunk.first()] = typedefCommands
+                        typedefCommands.addAll(knownSymbols)
+                        renamedInstructions[unknownSymbols.first()] = typedefCommands
                     }
             }else {
                 // Check if the user wants to execute a custom operation
-                if(instruction in userAliasOperations) {
-                    val result = evaluteCodeLine(userAliasOperations[instruction].toString().split(" "))
+                if(instruction in userOperations) {
+                    val result = evaluteCodeLine(userOperations[instruction].toString().split(" "))
                     resultList.addAll(result)
                 }
                 else {
